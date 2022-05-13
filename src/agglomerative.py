94c8d4f99a26a0a4bf4b1f1397fb0d2d8057e126
amr1235 <amr.ali99@eng-st.cu.edu.eg>
2022-05-13 23:04:22 +0200

    agglomerative done

--- src/agglomerative.py
+++ src/agglomerative.py
@@ -1,28 +1,32 @@
 import numpy as np
 import cv2 as cv
 import random
+import time
 
 class Agglomerative :
     def __init__(self,image_path) -> None:
         self.original_image = cv.imread(image_path)
-        self.image = np.array(cv.cvtColor(self.original_image,cv.COLOR_RGB2LUV))
+        self.image = np.array(self.original_image)
         self.points = self.image.reshape(self.image.shape[0] * self.image.shape[1] , 3)
-        self.distance_matrix = [[0]*len(self.points) for i in range(len(self.points))]
+        self.distance_matrix = [[-1]*len(self.points) for i in range(0,len(self.points))]
         self.dindogram = [[i] for i in range(len(self.points))]
 
     def calculate_initial_matrix(self) :
         for i in range(len(self.points)) :
             for j in range(i,len(self.points)) :
+                if(j == i) : 
+                    self.distance_matrix[j][i] = -1
+                    continue
                 self.distance_matrix[j][i] = self.calcualte_distance(self.points[i],self.points[j])
 
     def calcualte_distance(self,p1,p2) :
-        return float("{:.2f}".format(np.sqrt(np.sum(np.square(np.subtract(p1,p2)))),2))
+        return np.sqrt(np.sum(np.square(np.subtract(p1,p2))))
 
-    def get_minimum_distance(self,matrix):
+    def get_minimum_distance(self,matrix) :
         minimum = [1,0]
         for i in range(len(matrix)) :
             for j in range(len(matrix[0])) :
-                if(matrix[i][j] == 0.0) : continue
+                if((matrix[i][j] == -1)) : continue
                 if(matrix[i][j] < matrix[minimum[0]][minimum[1]]) :
                     minimum = [i,j]
         return minimum
@@ -30,7 +34,6 @@ class Agglomerative :
     def fit(self,number_of_clusters = 2) :
         if(number_of_clusters > len(self.points)) : raise Exception("You can not set the number of clusters to more than the number of points")
         self.calculate_initial_matrix()
-
         while len(self.dindogram) != number_of_clusters :
             minimum = self.get_minimum_distance(self.distance_matrix)
             new_cluster = [self.dindogram[minimum[0]],self.dindogram[minimum[1]]]
@@ -86,15 +89,15 @@ class Agglomerative :
                 indx = self.dindogram[i][j]
                 self.points[indx] = colors[i]
         image = self.points.reshape(self.original_image.shape)
-        cv.imshow(image)
-        cv.waitKey(0)
+        filename = "segmentedImage.png"
+        cv.imwrite(filename,image)
+        # cv.imshow("segmented image",image)
+        # cv.waitKey(0)
 
-
-agg = Agglomerative("../images/beach.jpg")
+############################### usage ###############################
+agg = Agglomerative("../images/ss.png")
+start_time = time.time()
+agg.fit(5)
+print("--- %s seconds ---" % (time.time() - start_time))
 agg.image_mask()
-# agg.fit(2)
-# agg.imageShow()
-
-# matrix = [[1,2,3],[1,2,3]]
-
-# print(len(matrix[0]))
+#####################################################################
